c8bdaf754d65a61c519ecd4d4d409e8eac779dca
oashtari <oashtari@gmail.com>
2020-05-03 18:08:35 -0700

    server up

--- api/server.js
+++ api/server.js
@@ -15,4 +15,8 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "it IS up" });
+});
+
 module.exports = server;
