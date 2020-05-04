a4fd8ab2c3f4fc555ba50f4fd2092ddfd0f7b683
oashtari <oashtari@gmail.com>
2020-05-03 20:25:40 -0700

    fixed error in jokes spec file

--- jokes/jokes-router.spec.js
+++ jokes/jokes-router.spec.js
@@ -37,26 +37,4 @@ describe("GET /api/jokes", () => {
             });
     });
 
-    // it("returns an array of jokes with valid token", async function () {
-    //     const newUser = { username: "gunner", password: "dogAgain" };
-    //     const { username, password } = newUser;
-
-    //     beforeEach(async () => {
-    //         await db("users").insert(newUser);
-    //     });
-
-    //     const loggedIn = await request(server)
-    //         .post("/api/auth/login")
-    //         .send({ username, password });
-
-    //     const { token } = loggedIn.body;
-
-    //     return request(server)
-    //         .get("/api/jokes")
-    //         .set("Authorization", token)
-    //         .then((res) => {
-    //             console.log('body', res.body)
-    //             expect(res.body).toHaveLength(20);
-    //         });
-    // });
 });
